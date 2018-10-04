6637cffac93dcb874d348cdfbfc93a165a03796b
Jenna Woodley <jenatalie@msn.com>
2018-10-04 22:02:31 +0000

    adding screenshots

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,70 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import NavBar from './NavBar.jsx';
 import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
 
 class App extends Component {
-   constructor(props) {
-    super(props);
-    this.state = {
-      id: [],
-      currentUser: {name: "Jenna"},
-      messages: [],
-      userCount: 0
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: [],
+            currentUser: { name: "Jenna" },
+            messages: [],
+            userCount: 0
+        };
+    }
 
-addMessage = content => {
-  if(content.username !== this.state.currentUser){
-    this.setState({currentUser: {name: content.username}})
-  }
+    addMessage = content => {
+        if (content.username !== this.state.currentUser) {
+            this.setState({ currentUser: { name: content.username } })
+        }
+        const message = {
+            username: this.state.currentUser.name,
+            content: content.content,
+            type: content.type
+        };
+        this.mySocket.send(JSON.stringify(message));
+    }
+    if (userCountUpdate) {
+        this.setState({ userCount: message.userCount })
+    }
 
-  const message = {
-    username: this.state.currentUser.name,
-    content:content.content,
-    type: content.type
-  };
-   this.mySocket.send(JSON.stringify(message));
-}
+    componentDidMount() {
+        console.log("componentDidMount <App />");
 
-if(userCountUpdate){
-  this.setState({userCount: message.userCount})
-}
+        this.mySocket = new WebSocket("ws://localhost:3001");
+        this.mySocket.onopen = (event) => {
+            // this.mySocket.send(messages);
+            console.log("connected!")
+        };
 
-componentDidMount() {
-  console.log("componentDidMount <App />");
+        this.mySocket.onmessage = (event) => {
+            const newMessage = JSON.parse(event.data);
+            console.log(newMessage)
+            const updatedMessages = this.state.messages.concat(newMessage);
 
-  this.mySocket = new WebSocket("ws://localhost:3001");
-  this.mySocket.onopen = (event) => {
-  // this.mySocket.send(messages);
-    console.log("connected!")
-  };
+            //compares type of usercount, sets state
 
+            if (newMessage.type === "userCountUpdate") {
+                this.setState({ userCount: newMessage.userCount });
+            } else {
+                this.setState({ messages: updatedMessages });
+            }
 
-  this.mySocket.onmessage = (event) => {
-    const newMessage = JSON.parse(event.data);
-        console.log(newMessage)
-      const updatedMessages = this.state.messages.concat(newMessage);
-
-      //compares type of usercount, sets state
-
-      if(newMessage.type === "userCountUpdate"){
-        this.setState({userCount: newMessage.userCount});
-      } else {
-        this.setState({messages: updatedMessages});
-      }
-
-      console.log(this.state.messages);
-
-
-    // switch (newMessage.type) {
-
-  //     case "userUpdate":
-  //     console.log("user has been updated")
-  //     break;
-
-  //     case "messageUpdate":
-  //     const updatedMessages = this.state.messages.concat(newMessage);
-  //     this.setState({messages: updatedMessages});
-  //     console.log("updated messages")
-  //     break;
-
-  //     default:
-  //     console.log(newMessage, "Whatttt are you doing?! Check your code lady.. ")
-  // }
- }
-}
+            console.log(this.state.messages);
+        }
+    }
 
 
-  render() {
-    return (
-      <div>
-  <NavBar userCount={this.state.userCount}/>
-  <MessageList messages={this.state.messages}/>
-  <ChatBar currentUser={this.state.currentUser.name} addMessage={this.addMessage}/>
-  </div>
-    );
-  }
+    render() {
+        return ( <div >
+            <NavBar userCount = { this.state.userCount }/>
+            <MessageList messages = { this.state.messages }/>
+            <ChatBar currentUser = { this.state.currentUser.name } addMessage = { this.addMessage }/>
+            </div>
+        );
+    }
 }
 
 
